6b386a405602f8cd6df878db67f53ec027d2f150
Yoav Bermand <yoav.bermand@gmail.com>
2019-09-17 23:03:26 +0300

    Email refill first version

--- src/App.js
+++ src/App.js
@@ -2,26 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: ""
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+
+                    </p>
+                    <input id='myinput' type='text'/>
+                    <button onClick={_ => {
+                        this.setState({email: document.getElementById('myinput').value})
+                    }}>
+                        Click Me
+                    </button>
+
+
+                    {this.state.email}
+
+
+                </header>
+            </div>
+        )
+    }
 }
 
 export default App;
